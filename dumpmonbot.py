192c02fb7aeddef6a5105d60dc0a4e041b847b51
Gonzalo Rafuls <grafuls@redhat.com>
2016-10-23 23:10:48 +0200

    Implemented Emailer
    Change-Id: I0d351057f2d06bdbefc3b9ff5b2633bdb0f596bc

--- dumpmonbot.py
+++ dumpmonbot.py
@@ -1,3 +1,4 @@
+from EmailManager import Emailer
 from tweepy import API
 from tweepy import Stream
 from tweepy import OAuthHandler
@@ -5,27 +6,40 @@ from tweepy.streaming import StreamListener
 
 import config
 import logging
-import os
 import re
 import urllib2
 
-LOGGER = logging.getLogger(__name__)
+LOGGER = logging.getLogger("DUMPMON")
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s |%(name)s| %(message)s',
+    filename='dumpmonbot.log'
+)
 
 
 class listener(StreamListener):
     def on_error(self, status):
-        print status
+        LOGGER.error(status)
 
     def on_status(self, status):
+        LOGGER.info(status.text)
         url = status._json['entities']['urls'][0]['expanded_url']
         html_content = urllib2.urlopen(url).read()
         regex = '(?:\A|(?<=.))[^.]*%s[^.]*(?:.|\Z)' % config.QUERY
         matches = re.findall(regex, html_content, 0)
+        msg = []
         if len(matches) > 0:
-            LOGGER.info("YOU HAVE BEEN PWND (potentially)")
-            LOGGER.info("Source: %s:", url)
+            HEADER = "YOU HAVE BEEN PWND (potentially)"
+            SOURCE_STRING = "Source: %s:"
+            msg.append(HEADER)
+            LOGGER.info(HEADER)
+            msg.append(SOURCE_STRING % url)
+            LOGGER.info(SOURCE_STRING, url)
             for match in matches:
+                msg.append(match)
                 LOGGER.info(match)
+            with Emailer(config.EMAILER_USER, config.EMAILER_PASS) as emailer:
+                emailer.send_email(msg='\n'.join(msg))
 
 
 if __name__ == "__main__":
