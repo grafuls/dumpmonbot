65803616c59986ae459d33388d382a3ee7edba5d
Gonzalo Rafuls <grafuls@redhat.com>
2016-10-23 16:34:24 +0200

    Refactored, added config and emailer
    Change-Id: I6c209664bbd5caa937bd49a961402682385aaa43

--- dumpmonbot.py
+++ dumpmonbot.py
@@ -3,26 +3,38 @@ from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 
-c_key = 'RhwBv3rqIMWWjCrNk4VAXysXm'
-c_secret = '0E9EfIb92rOoka9qItLlVJ23kt9PpI5G6CJTdA8GHCVdQJmQPE'
-a_token = '95932080-CgUYH2hwGBXt7CE4wCXL4bOHIuIcQhH7sJNxvZnSQ'
-a_secret = 'bzZxOwh1xTnhnBFdGm8atTS2oLA8o45pS4H1eAnD2iEvI'
+import config
+import logging
+import os
+import re
+import urllib2
+
+LOGGER = logging.getLogger(__name__)
+
 
 class listener(StreamListener):
     def on_error(self, status):
         print status
 
     def on_status(self, status):
-        print status.text
+        url = status._json['entities']['urls'][0]['expanded_url']
+        html_content = urllib2.urlopen(url).read()
+        regex = '(?:\A|(?<=.))[^.]*%s[^.]*(?:.|\Z)' % config.QUERY
+        matches = re.findall(regex, html_content, 0)
+        if len(matches) > 0:
+            LOGGER.info("YOU HAVE BEEN PWND (potentially)")
+            LOGGER.info("Source: %s:", url)
+            for match in matches:
+                LOGGER.info(match)
 
-auth = OAuthHandler(c_key, c_secret)
-auth.set_access_token(a_token, a_secret)
 
-api = API(auth)
+if __name__ == "__main__":
+    auth = OAuthHandler(config.C_KEY, config.C_SECRET)
+    auth.set_access_token(config.A_TOKEN, config.A_SECRET)
+    api = API(auth)
 
-user = api.get_user('grafuls08')
-api.update_status('#dumpmon')
+    user = api.get_user(config.T_USER)
+    user_id = str(user.id)
 
-twitterStream = Stream(auth, listener())
-twitterStream.filter(follow=[str(user.id)])
-
+    twitterStream = Stream(auth, listener())
+    twitterStream.filter(follow=[user_id])
