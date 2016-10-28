d0cd6391817b25008faa58726216a72b31b892a2
Gonzalo Rafuls <grafuls@gmail.com>
2016-10-28 13:28:39 +0200

    Fixed indent

--- dumpmonbot.py
+++ dumpmonbot.py
@@ -7,7 +7,6 @@ from tweepy.streaming import StreamListener
 import config
 import logging
 import re
-import urllib2
 import requests
 
 LOGGER = logging.getLogger("DUMPMON")
@@ -25,25 +24,25 @@ class listener(StreamListener):
     def on_status(self, status):
         LOGGER.info(status.text)
         url = status._json['entities']['urls'][0]['expanded_url']
-	r = requests.get(url)
-	if r.status_code == 200:
-        regex = '[^\n]*%s[^\n]*(?:.)' % config.QUERY
-        matches = re.findall(regex, r.text, 0)
-        msg = []
-        if len(matches) > 0:
-            HEADER = "YOU HAVE BEEN PWND (potentially)"
-            SOURCE_STRING = "Source: %s:"
-            msg.append(HEADER)
-            LOGGER.info(HEADER)
-            msg.append(SOURCE_STRING % url)
-            LOGGER.info(SOURCE_STRING, url)
-            for match in matches:
-                msg.append(match)
-                LOGGER.info(match)
-            with Emailer(config.EMAILER_USER, config.EMAILER_PASS) as emailer:
-                emailer.send_email(msg='\n'.join(msg))
-	else:
-		LOGGER.info("Failed to get content from %s", url)
+        r = requests.get(url)
+        if r.status_code == 200:
+            regex = '[^\n]*%s[^\n]*(?:.)' % config.QUERY
+            matches = re.findall(regex, r.text, 0)
+            msg = []
+            if len(matches) > 0:
+                HEADER = "YOU HAVE BEEN PWND (potentially)"
+                SOURCE_STRING = "Source: %s:"
+                msg.append(HEADER)
+                LOGGER.info(HEADER)
+                msg.append(SOURCE_STRING % url)
+                LOGGER.info(SOURCE_STRING, url)
+                for match in matches:
+                    msg.append(match)
+                    LOGGER.info(match)
+                with Emailer(config.EMAILER_USER, config.EMAILER_PASS) as emailer:
+                    emailer.send_email(msg='\n'.join(msg))
+        else:
+            LOGGER.info("Failed to get content from %s", url)
 
 
 if __name__ == "__main__":
